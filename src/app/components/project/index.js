1bdf8b0a52cfdc34485f15de0795329f860dfe5b
Kai Micah Mills <kaimicahmills@gmail.com>
2021-09-08 02:22:20 -0600

    fix warnings

--- src/app/components/project/index.js
+++ src/app/components/project/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Project = ({ name, url }) => {
 
   return (
-    <a className="project" href={url} target="_blank">
-      <img src={`https://s2.googleusercontent.com/s2/favicons?domain=${url}`} />
+    <a className="project" href={url} target="_blank" rel="noreferrer">
+      <img src={`https://s2.googleusercontent.com/s2/favicons?domain=${url}`} alt={url} />
       <p>{name}</p>
     </a>
   )
--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,7 @@ const Index = () => {
       <img src="/icon.svg" alt="Antimortality" />
       <p className="header"><strong>antimortality</strong><br/><br />towards eternal life for mankind<br /><span style={{ color: '#ff0000' }}>*this site is a very early work in progress*</span></p>
       <Tree />
-      <p className="footer">copyright &copy; 2021, a project by <a href="https://twitter.com/kaimicahmills" target="_blank">@kaimicahmills</a></p>
+      <p className="footer">copyright &copy; 2021, a project by <a href="https://twitter.com/kaimicahmills" target="_blank" rel="noreferrer">@kaimicahmills</a></p>
     </div>
   );
 };
