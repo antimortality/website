7e32973ffd37ae91fe1b523cf69043fe67491c30
Kai Micah Mills <kaimicahmills@gmail.com>
2022-08-25 16:43:58 -0600

    update readme and add github to footer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Antimortality
+
+To suggest a change, modify the data file in `src/app/data.js` and open a pull request.
+
+To run locally, `npm i` then `npm start`.
--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,7 @@ const Index = () => {
       <img src="https://kaimicahmills.com/assets/media/icon.png" alt="Antimortality" />
       <p className="header"><strong>Antimortality</strong><br/><br/>Towards eternal life for mankind.<br /><span style={{ color: '#ff0000' }}>*This site is a very early work in progress.*</span></p>
       <Tree />
-      <p className="footer">Copyright &copy; 2022 Antimortality; a project by <a href="https://twitter.com/kaimicahmills" target="_blank" rel="noreferrer">@kaimicahmills</a></p>
+      <p className="footer">Copyright &copy; 2022 Antimortality; a project by <a href="https://twitter.com/kaimicahmills" target="_blank" rel="noreferrer">@kaimicahmills</a>; contribute on <a href="https://github.com/antimortality/website" target="_blank" rel="noreferrer">Github</a>.</p>
     </div>
   );
 };
